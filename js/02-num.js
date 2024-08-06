7d2b8e3669cf2e231161669eeb71dc36ce151aad
Ausra123 <ausrausra@gmail.com>
2024-08-06 14:49:48 +0300

    skaiciai

--- js/02-num.js
+++ js/02-num.js
@@ -10,4 +10,23 @@ tikri skaicia, begalybės, NaN
 const teigimas = 5;
 const neigiamas = -8;
 const nulis = 0;
-const nulis= -0;
+const nulisMinus= -0;
+console.log(nulis, nulisMinus);
+
+const desimtainis = 3.141592564;
+console.log(desimtainis);
+
+const begalybe = Infinity;
+const begalybeMinus = -Infinity;
+console.log(begalybe, begalybeMinus);
+
+const nesamone = NaN;
+console.log(nesamone);
+ 
+/*const puseZodzio = 'labas' / 2;
+console.log(puse.puseZodzio);
+*/
+
+
+
+
