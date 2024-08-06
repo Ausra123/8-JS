2b2a99936472db2d70563e0fdc10aaaeb99237ab
Ausra123 <ausrausra@gmail.com>
2024-08-06 11:47:52 +0300

    JavaScript

---
+++ js/00-intro.js
@@ -0,0 +1,11 @@
+// numeruosime dvigubais skaičiai
+/*
+Konseptai:
+1) kintamieji - įsimena informaciją (pvz., vartotojo gimtadienis, gali būti vienas kintamasis, gali būti ir keli, pvz., metai, mėnesis, diena)
+2) sąlygos sakiniai - tarpusavio palyginimas (pvz., lygini gimimo datą ir įleidi pilnamečius į puslapį, arba jeigu uždirba tam tikrą atlyginimo dydį, naudojamas toks ar kitoks koeficientas)
+3) ciklai - darbas su pasikartojančio formato informacija, pasikartojančių procesų valdymas 
+4) funkcijos (procedūros) - uždara logika, pvz., vidurkis (sušeriame krūvą skaičių ir išspjauna rezultatą :) gauname apskaičiuotą vidurkį), tai reikalinga turėti vieną kodą ir naudojame pagal poreikį
+5) įvykiai - užsikrovė pulspais, paspausti mygtuką, pradėjo ar pabaigė siųsti failą, išsiuntė užklausą 
+6) oop (šeštas brolis) – objektinis programavimas – tai „skonio reikalas“ 😊.
+
+*/
